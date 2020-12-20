5159d7910704677063fc42b83e56351096b96604
Jeff-Adler <Jeffm.adler@gmail.com>
2020-12-20 14:29:31 -0500

    setup functional tests to expect two items in to-do list

--- functional_tests.py
+++ functional_tests.py
@@ -37,18 +37,19 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = self.browser.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
-
         # There is still a text box inverting her to add another item.
         # He types "Use cranial piercings to augment finger length."
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on the list.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+                      [row.text for row in rows])
 
         # Jefferson wonders whether the site will remember her list. Then he sees that
         # the site has generated a unique URL for him -- there is some explanatory text
@@ -57,6 +58,7 @@ class NewVisitorTest(unittest.TestCase):
         # He visits that URL - his to-do list is still there.
 
         # Satisfied, he goes back to sleep
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
